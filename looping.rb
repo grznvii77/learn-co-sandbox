1bf0a2daff79fd68bab7a6491b41adb5c3b60c4c
Alina Jiang <alina.jiang95@gmail.com>
2020-03-10 04:39:17 +0000

    Automatically backed up by Learn

---
+++ looping.rb
@@ -0,0 +1,19 @@
+# Fill out the content of the method 'looping'
+# to contain a 'loop' that 'puts' the phrase
+# "Never pass a school bus if it has flashing red lights"
+# an infinite number of times
+
+# Beneath your method definition, call your method 'looping'
+
+# Then run the file by typing 'ruby looping.rb'
+# Hit Control + C to exit the infinite loop
+
+def looping
+  loop do
+    puts "Never pass a school bus if it has flashing red lights"
+  end
+end  
+
+looping
+
+
